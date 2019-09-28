716fcb758eab49b8cd976d6b0909a2bf4b5ab64a
CXuTao <909108858@qq.com>
2019-09-28 14:39:11 +0800

    xt第一次提交

---
+++ project/person_recognitionApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PersonRecognitionappConfig(AppConfig):
+    name = 'person_recognitionApp'
---
+++ project/person_recognitionApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, re_path
+from . import views
+
+app_name = 'person_recognitionApp'
+
+urlpatterns = [
+    re_path(r'^index/$', views.index, name='index'),
+]
